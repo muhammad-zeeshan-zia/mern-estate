d60f7de07eff19313b340d853727378745007538
Muhammad Zeeshan Zia <95034881+zeeshihulk@users.noreply.github.com>
2024-03-07 19:31:09 +0500

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from '../models/user.model.js'
+import bcrypt from 'bcrypt';
+
+export const signup =async (req,res)=>{
+     const {username,email,password}=req.body;
+     const hashedPassword =  bcrypt.hashSync(password,10);
+   const newUser = new User({username,email,password:hashedPassword});
+   try{
+    await newUser.save();
+    res.status(201).json("User Created Successfully");
+   }
+   catch(err)
+   {
+   res.status(500).json(err.message);
+   }
+   
+}
--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 const app = express();
+app.use(express.json());
 dotenv.config();
 
-mongoose.connect("mongodb://127.0.0.1:27017").then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/Real-Estate").then(()=>{
     console.log("Database is Connected ")
 }).catch((err)=>{
     console.log(err);
@@ -23,3 +25,4 @@ app.listen(3000,()=>{
 
 
 app.use("/api/user",userRoute);
+app.use("/api/auth",authRouter);
